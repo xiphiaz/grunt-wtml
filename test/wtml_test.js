03d94ba5b80e1e960c626ad964dcba60f2674353
Zak Henry <zak.henry@gmail.com>
2013-10-07 20:49:44 +1100

    Added wtml functionality and tests

--- test/wtml_test.js
+++ test/wtml_test.js
@@ -3,46 +3,64 @@
 var grunt = require('grunt');
 
 /*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+ ======== A Handy Little Nodeunit Reference ========
+ https://github.com/caolan/nodeunit
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
+ Test methods:
+ test.expect(numAssertions)
+ test.done()
+ Test assertions:
+ test.ok(value, [message])
+ test.equal(actual, expected, [message])
+ test.notEqual(actual, expected, [message])
+ test.deepEqual(actual, expected, [message])
+ test.notDeepEqual(actual, expected, [message])
+ test.strictEqual(actual, expected, [message])
+ test.notStrictEqual(actual, expected, [message])
+ test.throws(block, [error], [message])
+ test.doesNotThrow(block, [error], [message])
+ test.ifError(value)
+ */
 
 exports.wtml = {
-  setUp: function(done) {
-    // setup here if necessary
-    done();
-  },
-  default_options: function(test) {
-    test.expect(1);
+    setUp: function (done) {
+        // setup here if necessary
+        done();
+    },
+//    default_options: function (test) {
+//        test.expect(1);
+//
+//        var actual = grunt.file.read('tmp/default_options');
+//        var expected = grunt.file.read('test/expected/default_options');
+//        test.equal(actual, expected, 'should describe what the default behavior is.');
+//
+//        test.done();
+//    },
+//    custom_options: function (test) {
+//        test.expect(1);
+//
+//        var actual = grunt.file.read('tmp/custom_options');
+//        var expected = grunt.file.read('test/expected/custom_options');
+//        test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+//
+//        test.done();
+//    },
+    extension_conversion: function (test) {
+        test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+        var actual = grunt.file.read('tmp/extension_test.tpl.html');
+        var expected = grunt.file.read('test/expected/extension_test.tpl.html');
+        test.equal(actual, expected, 'Basic .wtml to .tpl.html file extension change.');
 
-    test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
+        test.done();
+    },
+    basic_test: function (test) {
+        test.expect(1);
 
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+        var actual = grunt.file.read('tmp/basic_test.tpl.html');
+        var expected = grunt.file.read('test/expected/basic_test.tpl.html');
+        test.equal(actual, expected, 'Basic .wtml to .tpl.html conversion.');
 
-    test.done();
-  },
+        test.done();
+    }
 };
